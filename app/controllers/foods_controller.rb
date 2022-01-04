576b34ce8d5ee1f5de81049c97839ba9fbe8dc0c
kossi-stack <kossifioklou10@gmail.com>
2022-01-04 15:29:19 -0800

    Create method for food-controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,37 @@
 class FoodsController < ApplicationController
+load_and_authorize_resource
+before_action :set_food, only: [:show, :destroy]
+
+def index
+  @food = Food.new
+end
+
+def create
+    @food = current_user.foods.new(food_params)
+
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @food.destroy
+    respond_to do |format|
+      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
