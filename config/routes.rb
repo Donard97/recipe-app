92d7041f7d8464cf8c50a9aee62d1bab98024b05
Donard97 <donardgola@gmail.com>
2022-01-04 22:13:55 +0100

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipe_foods/index'
-  get 'recipe_foods/show'
-  get 'inventory_foods/index'
-  get 'inventory_foods/show'
-  get 'inventories/index'
-  get 'inventories/show'
-  get 'foods/index'
-  get 'foods/show'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "foods#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'foods#index'
+  resources :foods, only: [:index, :new, :create, :destroy]
 end
