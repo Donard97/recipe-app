faaf4ac77d9ef1fc56efb2d6a657dc136d8ac575
Donard97 <donardgola@gmail.com>
2022-01-07 21:02:51 +0100

    Fix rubocop errors

--- spec/requests/shopping_list_spec.rb
+++ spec/requests/shopping_list_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "ShoppingLists", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/shopping_list/index"
+RSpec.describe 'ShoppingLists', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/shopping_list/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/shopping_list/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/shopping_list/show'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
