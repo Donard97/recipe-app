c10f8f353a926b8a0c8417e01eb75ccbef4c5c31
Donard97 <donardgola@gmail.com>
2022-01-07 11:12:23 +0100

    Generate ShoppingListController

---
+++ app/controllers/shopping_list_controller.rb
@@ -0,0 +1,7 @@
+class ShoppingListController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,17 @@
 Rails.application.routes.draw do
+  get 'shopping_list/index'
+  get 'shopping_list/show'
   devise_for :users
   root to: 'foods#index'
-  resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
-    resources :recipe_foods, only: [:new]
+  resources :foods, only: [:index, :create, :new, :destroy]
+
+  resources :recipes, only: [:index, :show, :create, :new, :destroy] do
+    resources :recipe_foods, only: [:new, :destroy]
   end
-  resources :recipe_foods, only: [:edit, :update, :destroy, :create]
-  resources :public_recipes, only: %i[index] 
   
-  devise_scope :user do 
-    authenticated :user do 
-      root 'users#index', as: :authenticated_root 
-    end
-    unauthenticated do 
-      root 'devise/sessions#new', as: :unauthenticated_root 
-    end 
-  end
-end
-
+  resources :recipe_foods, only: [:edit, :update, :destroy, :create]
+ 
+  resources :public_recipes, only: %i[index]
 
   devise_scope :user do 
     authenticated :user do 
@@ -27,4 +21,6 @@ end
       root 'devise/sessions#new', as: :unauthenticated_root 
     end 
   end
+
+
 end
