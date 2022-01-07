4c231687583e36c363dce843c69af38bdeb53f69
kossi-stack <kossifioklou10@gmail.com>
2022-01-07 01:24:46 -0800

    Fix linters error

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
-    def index
-        @recipes = Recipe.where(public: true).order(updated_at: :desc)
-    end
+  def index
+    @recipes = Recipe.where(public: true).order(updated_at: :desc)
+  end
 end
