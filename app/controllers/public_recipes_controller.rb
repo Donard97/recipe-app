a2b1d3cd9d622dfa91d291c7f7790e62459fd707
kossi-stack <kossifioklou10@gmail.com>
2022-01-08 01:45:02 -0800

    Fix Undefined mothod error

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.where(public: true).order(updated_at: :desc)
+    @total_price = RecipeFood.where(recipe_id: @recipes.map(&:id)).map { |recipe_food| recipe_food.food.price * recipe_food.quantity }.sum
   end
 end
