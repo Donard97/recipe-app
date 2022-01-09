f76096821d89a766622fd1ae7b6f7a97266f151c
kossi-stack <kossifioklou10@gmail.com>
2022-01-08 16:14:07 -0800

    Add testing for requests

--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -1,17 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'RecipeFoods', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/recipe_foods/index'
-      expect(response).to have_http_status(:success)
+  include Devise::Test::IntegrationHelpers
+  let(:user) { User.create(name: 'Kolly', email: 'kolly@mail.com', password: 'password') }
+  let(:food) { user.foods.create(name: 'apple', measurement_unit: 'kg', price: 4) }
+  let(:recipe) do
+    user.recipes.create(name: 'Pizza', cookingTime: 1, preparationTime: 2, description: 'Try to test', public: false)
+    describe 'GET /index' do
+      before do
+        sign_in user
+        get new_recipe_recipe_food_path(recipe)
+      end
     end
-  end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/recipe_foods/show'
-      expect(response).to have_http_status(:success)
+    it 'should return response status correct (ok)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'respons to html' do
+      expect(response.content_type).to include 'text/html'
+    end
+
+    it 'should include correct placeholder' do
+      expect(response.body).to include('Add Recipe Food')
     end
   end
 end
